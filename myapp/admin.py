a8a0062017547c7cd29209c8086dd52bd5ce3f9f
SirBlast <juandaquelal14@gmail.com>
2023-01-18 15:27:30 -0500

    creation of models based on the database

---
+++ myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Company
+from .models import Location
+
+
+admin.site.register(Company)
+admin.site.register(Location)
+# Register your models here.
